eccc29368424a8584b580a7d8cd3e7fadca3cc36
dat nguyen <dat.nguyen@codeenginestudio.com>
2021-05-11 09:11:49 +0700

    [init/models] Update model transaction

--- lib/src/mocks/tag.dart
+++ lib/src/mocks/tag.dart
@@ -1,7 +1,8 @@
-import 'package:opalus/src/models/tag.dart';
+import 'package:opalus/src/models/core/tag.dart';
 
 List<Tag> mockTags = [
   Tag(id: '1', name: 'Tiền lương', loop: 'monthly'),
   Tag(id: '2', name: 'Đổ xăng'),
   Tag(id: '3', name: 'Ăn tối'),
+  Tag(id: '4', name: 'Nhậu'),
 ];
